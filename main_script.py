154e700d7b73ec99fe52d1f8da1432d9d648f349
Luis <luisperezdemiguelinfante@gmail.com>
2020-07-04 09:11:33 +0200

    uploading

--- main_script.py
+++ main_script.py
@@ -12,18 +12,22 @@ def argument_parser():
     return arg
 
 def main(country):
-    print("starting_pipeline")
+    print("working_on_it..")
     sqlitedb_path = 'data/raw/raw_data_project_m1.db'
     db_df = database.acquire(sqlitedb_path)
-    print('printing db_df', db_df.head())
+    #print('printing db_df', db_df.head())
     url = 'http://api.dataatwork.org/v1/jobs/autocomplete?contains=data'
     apis_df = apis.enrich_country_codes(url)
-    print('apis response', apis_df.head())
+    #print('apis response', apis_df.head())
     scrapping_url = 'https://ec.europa.eu/eurostat/statistics-explained/index.php/Glossary:Country_codes'
     scrapping_df = web_scrapping.including_scrapping(scrapping_url)
-    print('with_scrapping', scrapping_df.head())
-    merged_dataframe = gathering_cleaning.merging_all(db_df, apis_df, scrapping_df, country)
-    print(merged_dataframe)
+
+    if country is None:
+        merged_dataframe = gathering_cleaning.merging_all(db_df, apis_df, scrapping_df, country)
+        print(merged_dataframe)
+    else:
+        merged_dataframe = gathering_cleaning.merging_all_country(db_df, apis_df, scrapping_df, country)
+        print(merged_dataframe)
 
     merged_dataframe.to_csv(r'data/results/luis_perez.csv', index=False)
 
--- modules/gathering_cleaning.py
+++ modules/gathering_cleaning.py
@@ -13,12 +13,36 @@ def merging_all(db_df, api_df, scrapping_df, country):
     merge_scrapping = pd.merge(data_plus_api, scrapping_df, how='inner', on='country_code')
     merge_scrapping = merge_scrapping[["country", "suggestion", "gender"]]
     merge_scrapping["gender"].replace(to_replace = ['male', 'Fem', 'FeMale','female'], value = ['Male', 'Female', 'Female','Female'],inplace=True)
-    #if country != None:
-        #merge_scrapping = merge_scrapping.filter()
+
     count_column = merge_scrapping.groupby(['country', 'suggestion', 'gender']).agg({'suggestion': 'count'})
     count_column.columns = ['cantidad']
     final_df = count_column.reset_index()
 
     final_df['Percentage'] = (final_df['cantidad'] / final_df['cantidad'].sum()*100).round(2).astype(str) + '%'
+
+    return final_df
+
+
+def merging_all_country(db_df, api_df, scrapping_df, country):
+
+    #MERGE API
+    merge_api = pd.merge(db_df, api_df, how='inner', on='job')
+    data_plus_api = merge_api[["uuid", "country_code", "suggestion", "gender"]]
+
+    # MERGE CON EL SCRAPPING (deberia salir scrapping_df)
+    merge_scrapping = pd.merge(data_plus_api, scrapping_df, how='inner', on='country_code')
+    merge_scrapping = merge_scrapping[["country", "suggestion", "gender"]]
+    merge_scrapping["gender"].replace(to_replace = ['male', 'Fem', 'FeMale','female'], value = ['Male', 'Female', 'Female','Female'],inplace=True)
+
+    filter_country = merge_scrapping['country'] == country
+    merge_scrapping = merge_scrapping[filter_country]
+
+    count_column = merge_scrapping.groupby(['country', 'suggestion', 'gender']).agg({'suggestion': 'count'})
+    count_column.columns = ['cantidad']
+    final_df = count_column.reset_index()
+
+    final_df['Percentage'] = (final_df['cantidad'] / final_df['cantidad'].sum()*100).round(2).astype(str) + '%'
+
+
 
     return final_df
